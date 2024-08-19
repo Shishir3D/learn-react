2a12584d2181294c185b241fbf3ca14428250d5e
Shishir3D <shishirpoudel2061@gmail.com>
2024-08-19 15:27:53 +0545

    fix: updated some packages

--- ticTacToe/src/App.jsx
+++ ticTacToe/src/App.jsx
@@ -1,34 +1,72 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(false);
   const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    if (squares[i] != null){
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext){
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
   return (
     <>
       <div className="board-row">
-        <X value={squares[0]} />
-        <X value={squares[1]} />
-        <X value={squares[2]} />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
       </div>
       <div className="board-row">
-        <X value={squares[3]} />
-        <X value={squares[4]} />
-        <X value={squares[5]} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
       </div>
       <div className="board-row">
-        <X value={squares[6]} />
-        <X value={squares[7]} />
-        <X value={squares[8]} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
       </div>
     </>
   );
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 }
 
-function X({value}) {
+function Square({ value, onSquareClick }) {
   return (
     <>
       <button className="square relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-black dark:text-black focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
         <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white  rounded-md group-hover:bg-opacity-0">
-          {value}
+          <button className="square" onClick={onSquareClick}>
+            {value}
+          </button>
         </span>
       </button>
     </>
